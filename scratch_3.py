2abac977326a07c0b5e7309192399d95ca9227cf
serafim-78 <36924976+serafim-78@users.noreply.github.com>
2018-03-14 03:03:21 +0300

    Done?

---
+++ scratch_3.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+img=cv2.imread('gal_gadot.png', 0)
+height, width = img.shape[:2]
+#H-matrix
+H = np.array([[1.0/2, 1.0/2], [-1.0/2, 1.0/2]])
+for k in range(1,5):
+    if height>width:
+        L=np.zeros(shape=(height/2, width))
+        for i in range(0, height/2):
+            for j in range(0, width):
+                L[i, j] = np.dot(H, np.row_stack((img[2 *i,  j], img[2*i+1, j ])))[0]
+        height/=2
+    else:
+        L = np.zeros(shape=(height , width/ 2))
+        for i in range(0, height):
+            for j in range(0, width/2):
+                L[i, j] = np.dot(H, np.row_stack((img[i, 2 * j], img[i, 2 * j + 1])))[0]
+        width/=2
+    title="Image #"+k.__str__()
+    cv2.imshow(title,L)
+    cv2.waitKey(0)
