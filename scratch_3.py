bee47e1d317532735561c45a79b4aa9c31391347
serafim-78 <36924976+serafim-78@users.noreply.github.com>
2018-03-26 02:29:42 +0300

    Еще версия
    1. Разложение выведено в отдельную функцию
    2. Работа с цветным изображением
    3. Сохранение результата в текущую директорию

--- scratch_3.py
+++ scratch_3.py
@@ -1,30 +1,38 @@
 import cv2
 import numpy as np
-img=cv2.imread('gal_gadot.png', 0)
+
+def r_haar(imgs, div, height, width):
+    Haar = np.array([[1.0/2**0.5, 1.0/2**0.5], [-1.0/2**0.5, 1.0/2**0.5]])
+    for cim in range(0,div):
+        a=len(imgs)
+        for k in range(0,a):
+            hw=height>width
+            wh=1-hw
+            L=np.zeros(shape=(height/(2**hw), width/(2**wh)))
+            H=np.zeros(shape=(height/(2**hw), width/(2**wh)))
+            for i in range(0, height/2**hw):
+                for j in range(0, width/2**wh):
+                    L[i, j] = np.dot(Haar, np.row_stack((imgs[0][(2**hw) *i,  (2**wh)*j], imgs[0][(2**hw)*i+hw, (2**wh)*j+wh])))[0]
+                    H[i,j]= np.dot(Haar, np.row_stack((imgs[0][(2**hw) *i,  (2**wh)*j], imgs[0][(2**hw)*i+hw, (2**wh)*j+wh])))[1]
+            imgs.remove(imgs[0])
+            imgs.append(L)
+            imgs.append(H)
+        height /= 2 ** hw
+        width /= 2 ** wh
+    return imgs
+
+img=cv2.imread('gal_gadot.jpg', 1)
 height, width = img.shape[:2]
-imgs=[img]
-#H-matrix
-Haar = np.array([[1.0/2**0.5, 1.0/2**0.5], [-1.0/2**0.5, 1.0/2**0.5]])
+B, G, R= [img[:,:,0]], [img[:,:,1]], [img[:,:,2]]
 print "Please, set count of division (>0). Advice 2:"
 div=int(input())
 print "Please, stand by..."
-for cim in range(0,div):
-    a=len(imgs)
-    for k in range(0,a):
-        hw=height>width
-        wh=1-hw
-        L=np.zeros(shape=(height/(2**hw), width/(2**wh)))
-        H=np.zeros(shape=(height/(2**hw), width/(2**wh)))
-        for i in range(0, height/2**hw):
-            for j in range(0, width/2**wh):
-                L[i, j] = np.dot(Haar, np.row_stack((imgs[0][(2**hw) *i,  (2**wh)*j], imgs[0][(2**hw)*i+hw, (2**wh)*j+wh])))[0]
-                H[i,j]= np.dot(Haar, np.row_stack((imgs[0][(2**hw) *i,  (2**wh)*j], imgs[0][(2**hw)*i+hw, (2**wh)*j+wh])))[1]
-        imgs.remove(imgs[0])
-        imgs.append(L)
-        imgs.append(H)
-    height /= 2 ** hw
-    width /= 2 ** wh
-for i in range(0, len(imgs)):
-    title="C:/Image#"+(i+1).__str__()+".png"
-    cv2.imwrite(title, imgs[i])
-print "Images saved on disk C"
+massB=r_haar(B, div, height, width)
+print "33%..."
+massG=r_haar(G, div, height, width)
+print "66%..."
+massR=r_haar(R, div, height, width)
+print "99%..."
+for i in range(0, 4):
+    k=cv2.merge((massB[i], massG[i],massR[i]))
+    cv2.imwrite("Img#"+(i+1).__str__()+".jpg", k)
